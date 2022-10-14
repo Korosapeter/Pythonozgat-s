f736ea19f22c99841f79f46ba1e80d20dd3e602a
korosa péter <97808318+Korosapeter@users.noreply.github.com>
2022-10-14 14:48:16 +0200

    Add files via upload
    2022.10.14
    Bolondoztunk csak csókával

---
+++ 2022.10.14.Csokas.bolondozas.masolata.py
@@ -0,0 +1,96 @@
+
+import random
+"""1
+szam=input("Adj meg egy számot")
+if szam%3==0  
+   print("0osztható")
+if szam%4==0
+    print("0osztható")
+if szam%9==0:  
+    print("0osztható")
+else:    
+    print("nem osztható")"""
+
+"""2
+szam=int(input("Add meg a víz hőmérsékletét"))
+if szam>=0:
+    print("Szilárd halmazállapotu")
+if szam>100 and szam<0:
+    print("Folyékony")
+if szam<100:
+    print("Gáz")"""
+
+"""3
+szam1=int(input("Adj meg egy számot(mettől)"))
+szam2=int(input("Adj meg egy számot(Meddig)"))
+for i in range(3):
+    print(random.randint<szam1 and random.randint>szam2)"""
+
+""""4
+hossz=60
+darab=int(input("Add meg a dinnyék darabját"))
+hossz=int(input("Add meg a dinnye átmérőjét"))
+print(darab*(hossz*2)/100("Méret kell a dinnyékhez"))
+"""
+"""5 nincs kész👎
+bekér1=int(input("Addj meg egy időpontot"))
+bekér2=int(input("Addj meg egy időpontot"))
+bekér=bekér1.split(",")
+idő=bekér2.split(",")
+kiírás=bekér2[0]*60+idő[1]*60+idő[2]
+kiírás1=bekér2[0]*60+idő[1]*60+idő[2]
+print(kiírás,kiírás1)
+"""
+"""6
+a=int(input("Adj meg egy számot"))
+b=int(input("Adj meg egy számot"))
+c=int(input("Adj meg egy számot"))
+if a==b==c:
+    print("Egyenlő oldalu háromszög")
+else:
+    print("Nem egyenlő oldalu")
+if a==b or a==c or c==b:
+     print("Egyenlő száru háromszög")
+else:
+    print("Nem egyenlő száru háromszög")"""
+""""7
+hatos=0
+számok=[]
+for i in range(10):
+    számok.append(random.randint(0,6))
+    for i in számok:
+        if i ==6:
+            hatos+=1
+            print(f"Hatosok száma{hatos}")
+"""
+"""8
+valami=int(input("Adj meg egy mondatot"))
+for i in valami:
+    print(i,end")
+"""
+"""9
+eletkor=int(input("Add meg az életkort"))
+if eletkor<7:
+    print("gyerek")
+if eletkor>7 and eletkor<23:
+    print("iskolás")
+if eletkor>23 and eletkor<65:
+    print("felnőtt")
+if eletkor>64:
+    print("nyugdíjas")
+"""
+"""10
+int(input("Adj megy egy számot"))
+a=+1
+b=+1
+eredmény=0
+eredmény += a*b
+print(f"{a}*{b}")
+print(f"Eredmény{eredmény}")
+"""
+"""11
+lista=[]
+while 0 not in lista:
+    lista.append(int(input("Adj meg egy egész számot")))
+lista.sort(lista[-1],"volt a legnagyobb számod")
+"""
